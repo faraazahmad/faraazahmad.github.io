cc818692b16c5cc570bb8ee527890f895179ae6d
Syed Faraaz Ahmad <faraaz98@live.com>
2018-05-20 00:01:09 +0530

    add interests

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 var typed = new Typed('.typed', {
   strings: ["Contribute to open source", "Write blog posts on programming", "apply deep learning to real life"],
   typeSpeed: 30,
+  loop: true,
   cursorChar: '█'
 });
