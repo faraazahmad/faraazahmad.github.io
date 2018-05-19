9e27e5e22317ab75d1f7f3ccd1ad986ae2acdc50
Syed Faraaz Ahmad <faraaz98@live.com>
2018-05-19 23:10:34 +0530

    add typewriter animation

---
+++ js/home.js
@@ -0,0 +1,5 @@
+var typed = new Typed('.typed', {
+  strings: ["Contribute to open source", "Write blog posts on programming", "apply deep learning to real life"],
+  typeSpeed: 30,
+  cursorChar: '█'
+});
