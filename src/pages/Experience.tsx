5ba19cacbf72c5152f59bde1b75ea19e14a437ac
Syed Faraaz Ahmad <faraaz@Syeds-MacBook-Pro.local>
2023-09-06 20:00:46 +0530

    add optum experience

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -11,7 +11,55 @@ function Experience() {
       <div className="block my-5 w-3/4">
         <p className="my-10 bg-white p-10 border-solid border-4 border-champagne-200 hover:border-champagne-500 rounded-lg">
           <h3 className="text-xl">
-            Software Engineer at{" "}
+            Software Engineer at
+            <a
+              target="_blank"
+              href="https://deepsource.com"
+              className="text-terracotta font-bold"
+            >
+              DeepSource
+            </a>
+          </h3>
+          <span className="font-mono">Sep 2022 - Sep 2023</span>
+          <br />
+          <br />
+          DeepSource is a code health company that statically analyzes your code and finds quality and secuity issues.
+          <br />
+          <ul className="list-disc">
+            <li>
+              Led development of best-in-class Ruby static analyzer that detects 300+ code-quality & security issues
+			  in Ruby/Rails codebases.
+            </li>
+            <li>
+              Achieved 15x performance improvement in Ruby static analyzer using extensive benchmarking, 
+			  CPU profiling and caching.
+            </li>
+            <li>
+				Built new techniques using static analysis and migration-stitching to extract semantic information from Ruby on Rails applications.
+			</li>
+            <li>
+              Integrated real-time performance monitoring of services using Grafana
+            </li>
+            <li>
+				Led re-architecture and development of secrets scanning analyzer that automatically detects and 
+				flags 100+ hardcoded API tokens.
+            </li>
+            <li>
+              Created automated performance testing for static analyzers that runs in self-hosted Github actions.
+            </li>
+			<li>
+				Created a graphical interface for MISRA-C code compliance reports.
+			</li>
+          </ul>
+
+			<span className="font-bold">
+				Skills: Vue, Ruby on Rails, GraphQL, Ruby, Golang, Docker, Grafana
+			</span>
+        </p>
+
+        <p className="my-10 bg-white p-10 border-solid border-4 border-champagne-200 hover:border-champagne-500 rounded-lg">
+          <h3 className="text-xl">
+            Software Engineer at
             <a
               target="_blank"
               href="https://optum.com"
@@ -20,7 +68,7 @@ function Experience() {
               Optum
             </a>
           </h3>
-          <span className="font-mono">Aug 2020 - Present</span>
+          <span className="font-mono">Aug 2020 - Sep 2022</span>
           <br />
           <br />
           Optum is a company that builds products and services for UnitedHealth
@@ -28,25 +76,28 @@ function Experience() {
           <br />
           <ul className="list-disc">
             <li>
-              Built Backend with microservice architecture using Spring Boot
+              Improved frontend load times by 14% by reducing bundle size and caching requests using service workers.
             </li>
             <li>
-              Built micro frontends in React, which share a common session using
-              Redis
-            </li>
-            <li>Took initiative to add UI integration testing using Cypress</li>
-            <li>
-              Improved frontend performance using Workbox service worker library
+              Built advanced search to improve the discoverability of fields in insurance forms.
             </li>
             <li>
-              Used code splitting and tree shaking to reduce frontend bundle
-              sizes and improve website load times
+				Worked on an internal tool to instantly notify multiple developer teams of database schema changes
+			</li>
+            <li>
+              Mentored a team of junior developers to identify areas of improvement and growth
             </li>
             <li>
-              Took initiatives to make the codebase more streamlined by
-              introducing standardised code style to the team
+              Used code splitting and tree shaking to reduce frontend bundle sizes and improve website load times
+            </li>
+            <li>
+              Integrated event-based microservices by standardizing API results
             </li>
           </ul>
+
+			<span className="font-bold">
+				Skills: Java, Spring Boot, MySQL, TypeScript, React, Angular
+			</span>
         </p>
 
         <p className="my-10 bg-white p-10 border-solid border-4 border-champagne-200 hover:border-champagne-500 rounded-lg">
