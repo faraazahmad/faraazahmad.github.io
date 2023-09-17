b75e570bf55da96ceb1094e0dd6704aeef8994ad
Syed Faraaz Ahmad <faraaz98@live.com>
2023-09-17 13:12:06 +0530

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/v1/',
-  publicDir: '/v1/'
+  base: '',
+  publicDir: ''
 })
